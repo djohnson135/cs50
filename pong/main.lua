8cd0b3e15df01e567d731cacafb4cc41c6a8ac14
djohnson135 <Dathanljohnson135@gmail.com>
2020-06-07 17:53:05 -0500

    replaced functions with push library

--- pong/main.lua
+++ pong/main.lua
@@ -1,6 +1,12 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false; --[[windowed]]
@@ -8,7 +14,11 @@ function love.load()
         resizable = false; --[[aspect ratio not changeable]]
     })
 end 
-
+function love.keypressed(key)
+    if key == 'escape' then
+            love.event.quit()
+    end
+end
 function love.draw()
     love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center') --[[this is a comment]]
 end
